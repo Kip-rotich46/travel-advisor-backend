f07408b9e9f301e2a74fa7d1201f68f3713d0d9c
protio <protiolangat468@gmail.com>
2024-08-09 21:06:47 +0300

    Backend done

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import { verifyAdmin } from "../utils/verifyToken.js";
+import { createRoom, deleteRoom, getRooms, getSingleRoom, updateRoom } from '../controllers/roomController.js';
 
 const router = express.Router();
 
+//CREATE
+router.post("/:hotelid",verifyAdmin, createRoom);
 
+//UPDATE
+router.put("/:id",verifyAdmin, updateRoom);
+
+//DELETE
+router.delete("/:id",verifyAdmin, deleteRoom);
+
+//GET
+router.get("/:id", getSingleRoom);
+
+//GET ALL
+router.get("/", getRooms);
 
 export default router;
+
+
